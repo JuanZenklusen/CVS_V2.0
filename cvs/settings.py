37a26e69b829cd048101d25d0351709528b78175
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-08-29 18:07:23 -0300

    cambio de puerto

--- cvs/settings.py
+++ cvs/settings.py
@@ -94,7 +94,8 @@ DATABASES = {
         "USER": os.getenv('DB_USER'),
         "PASSWORD": os.getenv('DB_PASS'),
         "HOST": "127.0.0.1",
-        "PORT": "5433",
+        #"PORT": "5433",
+        "PORT": "5432",
     }
 }
 
